c221545f24b232ead85db9c12237b27c8b6813d1
Huaijin Li <578761491@qq.com>
2024-03-02 18:56:34 +0800

    重构前端框架，增加适配性测试UI

--- src/main/java/backend/tester/fileSystem/AdaptTester.java
+++ src/main/java/backend/tester/fileSystem/AdaptTester.java
@@ -1,9 +1,20 @@
 package backend.tester.fileSystem;
 
+import backend.dataset.TestArguments;
 import backend.dataset.TestResult;
 import backend.tester.TestItem;
+import frontend.connection.DBConnection;
+import frontend.connection.SSHConnection;
 
 public class AdaptTester extends TestItem {
+
+    public AdaptTester(String testName, SSHConnection sshStmt, DBConnection DBStmt) {
+        super(testName, sshStmt, DBStmt);
+    }
+
+    public AdaptTester(String testName, SSHConnection sshStmt, DBConnection DBStmt, TestArguments testArgs) {
+        super(testName, sshStmt, DBStmt, testArgs);
+    }
 
     @Override
     public void dataPrepare() {
