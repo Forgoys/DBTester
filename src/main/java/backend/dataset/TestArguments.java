aeb8d59516fa40f995980cb5950885f0410f2f87
cx <cx22757@163.com>
2024-03-02 17:37:01 +0800

    完善了数据集类。 TODO：各成员仿照已有代码填写TestArguments、TestResult、TestTimeData三个类中的常量字段。

--- src/main/java/backend/dataset/TestArguments.java
+++ src/main/java/backend/dataset/TestArguments.java
@@ -3,38 +3,6 @@ package backend.dataset;
 import java.util.ArrayList;
 import java.util.List;
 
-
-class ArgumentProperty {
-    /**
-     * 参数名称
-     */
-    public String argNames;
-
-    /**
-     * 候选项的值
-     */
-    public String[] candidateValues = null;
-
-    /**
-     * 判断该参数是否有候选项
-     */
-    public boolean hasCandidate() {
-        if(candidateValues == null) {
-            return false;
-        }
-        return candidateValues.length != 0;
-    }
-
-    public ArgumentProperty(String argNames) {
-        this.argNames = argNames;
-    }
-
-    public ArgumentProperty(String argNames, String[] candidateValues) {
-        this.argNames = argNames;
-        this.candidateValues = candidateValues;
-    }
-}
-
 /**
  * 测试参数抽象类，需要定义测试参数子类继承该类
  */
